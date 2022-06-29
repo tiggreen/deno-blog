a6b89c781ff3151db1f6af73855dcd06cd99d3a8
Tigran Hakobyan <thakobyan@netflix.com>
2022-06-29 19:23:01 -0400

    initial commit

---
+++ routes/about.tsx
@@ -0,0 +1,12 @@
+// routes/about.tsx
+
+/** @jsx h */
+import { h } from 'preact';
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
